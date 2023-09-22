8525964c27d9d9179bcc54d9f4759f10e1dc1f84
Ameto Eklu <eameto589@gmail.com>
2023-09-22 04:10:05 -0400

    Assignment-4-part2-sucess

---
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -0,0 +1,32 @@
+
+##############################################################
+#
+# AESD-ASSIGNMENTS
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+# I used git log to find this commit version
+AESD_ASSIGNMENTS_VERSION = 16a988d3ea5c0cdbe41007d1312a77463aec3292 
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-pclumson.git
+AESD_ASSIGNMENTS_SITE_METHOD = git
+AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+
+define AESD_ASSIGNMENTS_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+endef
+
+# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
+	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+endef
+
+$(eval $(generic-package))
---
+++ clean.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+make -C ./buildroot distclean
+
+# cd $(dirname $0)
+
+# if [ -d buildroot ]
+
+# then
+#         make -C buildroot distclean 
+
+# else
+#         echo "error: buildroot is not installed in your system !" 
+
+# fi
